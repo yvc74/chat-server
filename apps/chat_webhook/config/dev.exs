1b38e04de82c6ac51559669f3f91f03246638817
yvc74 <43711816+yvc74@users.noreply.github.com>
2019-05-31 08:56:57 +0300

    Update dev.exs

--- apps/chat_webhook/config/dev.exs
+++ apps/chat_webhook/config/dev.exs
@@ -5,5 +5,6 @@ config :chat_webhook, ChatWebhook.Repo,
   hostname: System.get_env("CHAT_WEBHOOK_POSTGRES_HOST"),
   username: System.get_env("CHAT_WEBHOOK_POSTGRES_USER"),
   password: System.get_env("CHAT_WEBHOOK_POSTGRES_PASS"),
-  database: System.get_env("CHAT_WEBHOOK_POSTGRES_DB"),
+  database: System.get_env("CHAT_SERVER_POSTGRES_DB") <> "_dev",
+  pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
