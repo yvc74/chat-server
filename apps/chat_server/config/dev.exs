1e73ce30d9f0b7c56b84495818e8e9895a031488
yvc74 <43711816+yvc74@users.noreply.github.com>
2019-05-31 08:47:04 +0300

    Update dev.exs

--- apps/chat_server/config/dev.exs
+++ apps/chat_server/config/dev.exs
@@ -8,6 +8,6 @@ config :chat_server, ChatServer.Repo,
   hostname: System.get_env("CHAT_SERVER_POSTGRES_HOST"),
   username: System.get_env("CHAT_SERVER_POSTGRES_USER"),
   password: System.get_env("CHAT_SERVER_POSTGRES_PASS"),
-  database: System.get_env("CHAT_SERVER_POSTGRES_DB"),
+  database: System.get_env("CHAT_SERVER_POSTGRES_DB") <> "_dev",
   pool: Ecto.Adapters.SQL.Sandbox
   pool_size: 10
