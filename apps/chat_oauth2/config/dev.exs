64b15950d455d17cd203158db1c0765f4f75bb67
yvc74 <43711816+yvc74@users.noreply.github.com>
2019-05-31 08:56:17 +0300

    Update dev.exs

--- apps/chat_oauth2/config/dev.exs
+++ apps/chat_oauth2/config/dev.exs
@@ -43,7 +43,8 @@ config :chat_oauth2, ChatOAuth2.Repo,
   hostname: System.get_env("CHAT_OAUTH2_POSTGRES_HOST"),
   username: System.get_env("CHAT_OAUTH2_POSTGRES_USER"),
   password: System.get_env("CHAT_OAUTH2_POSTGRES_PASS"),
-  database: System.get_env("CHAT_OAUTH2_POSTGRES_DB"),
+  database: System.get_env("CHAT_SERVER_POSTGRES_DB") <> "_dev",
+  pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
 # Turn on GraphiQL
